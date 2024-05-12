6dbe9a2c461395cbed4c3dd4c21e561fafb02e7f
Iyam <flutteriam@gmail.com>
2024-05-13 03:10:39 +0700

    update user message table

--- app/Models/UserMessage.php
+++ app/Models/UserMessage.php
@@ -7,5 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserMessage extends Model
 {
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'user_message';
+
     use HasFactory;
 }

0a496e69f4d7b7c0fb23dc1e75c67f8e7666a1a0
Iyam <flutteriam@gmail.com>
2024-05-13 19:39:44 +0700

    ignore record on edit admin

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -45,7 +45,7 @@ class UserResource extends Resource
                     ->imageEditorAspectRatios(['1:1']),
                 Forms\Components\TextInput::make('email')
                     ->email()
-                    ->unique()
+                    ->unique(ignoreRecord: true)
                     ->required()
                     ->maxLength(255),
                 Forms\Components\TextInput::make('password')
